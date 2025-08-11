84a8b235cdcc821c0d280bf6a07193a6fa1cc6c1
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2025-08-11 11:21:18 +0530

    Time: 5 ms (79.04%) | Memory: 45.1 MB (76.22%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // adjacency list
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // indegree array
+        int[] indegree = new int[numCourses];
+
+        // build graph
+        for (int i = 0; i < prerequisites.length; i++) {
+            int u = prerequisites[i][1];
+            int v = prerequisites[i][0];
+            adj.get(u).add(v);
+            indegree[v]++;
+        }
+
+        // queue for BFS
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int count = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            count++;
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.add(it);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+}
