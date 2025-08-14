0bb60f28ada75202ae59a922c9fd798ebbc530db
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2025-08-14 16:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (64.20%) - LeetSync

---
+++ 933-increasing-order-search-tree/increasing-order-search-tree.java
@@ -0,0 +1,26 @@
+class Solution {
+    Stack<TreeNode> st = new Stack<>();
+    
+    public TreeNode increasingBST(TreeNode root) {
+        helper(root);
+
+        TreeNode head = st.pop();
+        TreeNode curr = head;
+
+        while (!st.isEmpty()) {
+            TreeNode next = st.pop();
+            next.left = null;
+            next.right = null; // clear old links
+            curr.right = next;
+            curr = next;
+        }
+        return head;
+    }
+
+    public void helper(TreeNode root) {
+        if (root == null) return;
+        helper(root.right);
+        st.push(root);
+        helper(root.left);
+    }
+}
