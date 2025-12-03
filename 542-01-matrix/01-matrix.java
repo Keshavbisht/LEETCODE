392fdc427cb22d7cd4619abfa2d67f45dc318c0c
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2025-12-03 20:50:16 +0530

    Time: 15 ms (40.45%) | Memory: 49.3 MB (18.32%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,52 +1,47 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        // List<List<Integer>> mapZero = new ArrayList<>();
-        int m = mat.length;
-        int n = mat[0].length;
+        int n = mat.length;
+        int m = mat[0].length;
+        int[][] vis = new int[n][m];
+        for(int[] i: vis){
+            Arrays.fill(i, -1);
+        }
         Queue<int[]> q = new LinkedList<>();
-        int[][] vis = new int[m][n];
-
-        for(int a = 0; a< mat.length; a++){
-            for(int b = 0; b<mat[0].length; b++){
-                if(mat[a][b] == 0){
-                    q.add(new int[]{a,b});
-                    vis[a][b] = 1;
+        for(int i = 0; i< mat.length; i++){
+            for(int j = 0; j< mat[0].length; j++){
+                if(mat[i][j] == 0){
+                    q.add(new int[]{i,j});
+                    vis[i][j] = 1;
                 }
             }
         }
 
         while(!q.isEmpty()){
-
             int[] curr = q.remove();
-            
-            
             int i = curr[0];
             int j = curr[1];
-                
-
-            if(i-1>=0 && vis[i-1][j] == 0){
-                mat[i-1][j] = mat[i][j] +1;
-                vis[i-1][j] = 1;
-                q.add(new int[]{i-1,j});
-            }
-            if(i+1<m && vis[i+1][j] == 0){
-                mat[i+1][j] = mat[i][j] +1;
-                vis[i+1][j] = 1;
-                q.add(new int[]{i+1,j});
-            }
-            if(j-1>=0 && vis[i][j-1] == 0){
-                mat[i][j-1] = mat[i][j] +1;
-                vis[i][j-1] = 1;
-                q.add(new int[]{i,j-1});
-            }
-            if(j+1<n && vis[i][j+1] == 0){
-                mat[i][j+1] = mat[i][j] +1;
-                vis[i][j+1] = 1;
-                q.add(new int[]{i,j+1});
-            }
             
+            if(i-1 >= 0 && vis[i-1][j] == -1){
+                mat[i-1][j] = mat[i][j] +1;
+                q.add(new int[]{i-1, j});
+                vis[i-1][j] = 1;
+            }
+            if(i+1 <n && vis[i+1][j] == -1){
+                mat[i+1][j] = mat[i][j] +1;
+                q.add(new int[]{i+1, j});
+                vis[i+1][j] = 1;
+            }
+            if(j-1 >= 0 && vis[i][j-1] == -1){
+                mat[i][j-1] = mat[i][j] +1;
+                q.add(new int[]{i, j-1});
+                vis[i][j-1] = 1;
+            }
+            if(j+1 < m && vis[i][j+1] == -1){
+                mat[i][j+1] = mat[i][j] +1;
+                q.add(new int[]{i, j+1});
+                vis[i][j+1] = 1;
+            }
         }
-
         return mat;
     }
 }
