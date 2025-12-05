ed64b0b07754d249014c7de5bdcbbcd944e28863
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2025-12-05 23:35:49 +0530

    Time: 3 ms (99.34%) | Memory: 46.4 MB (54.30%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,47 +1,47 @@
 class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
+    Stack<Integer> st;
+    boolean[] vis;
+    boolean[] check;
+    public int[] findOrder(int numCourses, int[][] arr) {
+        st = new Stack<>();
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
+        for(int i = 0; i< numCourses; i++){
             adj.add(new ArrayList<>());
         }
-
-        int[] indegree = new int[numCourses];
-
-        
-        for (int i = 0; i < prerequisites.length; i++) {
-            int u = prerequisites[i][1];
-            int v = prerequisites[i][0];
-            adj.get(u).add(v);
-            indegree[v]++;
+        for(int i = 0; i< arr.length; i++){
+            int u = arr[i][0];
+            int v = arr[i][1];
+            adj.get(v).add(u);
         }
-
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
+        vis = new boolean[numCourses];
+        check = new boolean[numCourses];
+        for(int i = 0; i< numCourses; i++){
+            if(!vis[i]){
+                if(!helper(i, adj)) return new int[]{};
             }
         }
+        int[] ans = new int[st.size()];
+        int i = 0;
+        while(!st.isEmpty()){
+            ans[i] = st.pop();
+            i++;
+        }
+        return ans;
 
-        int[] order = new int[numCourses];
-        int index = 0;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            order[index++] = node; 
-
-            for (int it : adj.get(node)) {
-                indegree[it]--;
-                if (indegree[it] == 0) {
-                    q.add(it);
-                }
+    }
+    public boolean helper(int ind, ArrayList<ArrayList<Integer>> adj){
+        vis[ind] = true;
+        check[ind] = true;
+        for(int i = 0; i< adj.get(ind).size(); i++){
+            if(check[adj.get(ind).get(i)]) return false;
+            
+            
+            if(!vis[adj.get(ind).get(i)]){
+                if(!helper(adj.get(ind).get(i), adj)) return false;
             }
         }
-
-        
-        if (index == numCourses) {
-            return order;
-        } else {
-            return new int[0];
-        }
+        st.push(ind);
+        check[ind] = false;
+        return true;
     }
 }
