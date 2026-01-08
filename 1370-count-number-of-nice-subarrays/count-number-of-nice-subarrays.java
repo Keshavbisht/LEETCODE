47188814106e2e2501d345af51a15daa58ea6fc9
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-01-08 22:58:24 +0530

    Time: 47 ms (5.88%) | Memory: 55.7 MB (93.88%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,35 +1,33 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int n = nums.length;
-        for(int i = 0; i< n; i++){
-            if(nums[i]%2 == 0){
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i] % 2 == 0){
                 nums[i] = 0;
             }
             else{
                 nums[i] = 1;
             }
         }
-
-        return helper(nums, k) - helper(nums, k-1);
-
-    }
-    public int helper(int[] nums, int goal){
-        int a = 0;
-        int b = -1;
-        int n = nums.length;
-        int temp = 0;
+        int pnt2 = -1;
+        int pnt1 = 0;
         int ans = 0;
-        while(b< n-1){
-            b++;
-            temp = temp + nums[b];
-            while(a<=b && temp > goal){
-                if(nums[a] == 1){
-                    temp--;
-                }
-                a++;
+        int prefix = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        while(pnt2<nums.length -1){
+            pnt2++;
+            prefix = prefix + nums[pnt2];
+            if(map.containsKey(prefix - k)){
+                ans = ans + map.get(prefix - k);
             }
-            ans = ans + (b-a +1);
+            if(map.containsKey(prefix)){
+                map.put(prefix, map.get(prefix)+1);
+            }
+            else{
+                map.put(prefix, 1);
+            }
         }
         return ans;
+        
     }
 }
