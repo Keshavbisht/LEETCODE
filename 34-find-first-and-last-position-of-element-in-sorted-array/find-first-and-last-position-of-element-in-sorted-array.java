5ae1fac2c4505bf6f910b687854ddcb9a2a3779b
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-01-11 23:45:05 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (33.92%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -4,15 +4,46 @@ class Solution {
         ans[0] = -1;
         ans[1] = -1;
         int prev = -1;
-        for(int i = 0; i< nums.length; i++){
-            if(ans[0] == -1 && nums[i]==target){
-                ans[0] = i;
-            }
-            if(nums[i] == target){
-                prev = i;
-            }
+        ans[0] = binary(nums, 0, nums.length-1, target);
+        if(ans[0] == Integer.MAX_VALUE){
+            ans[0] = -1;
         }
-        ans[1] = prev;
+        ans[1] = binaryG(nums, 0, nums.length-1, target);
         return ans;
+        
+    }
+    public int binary(int[] nums,int low, int high, int target){
+        // int low = 0;
+        // int high = nums.length-1;
+        if(low > high) return Integer.MAX_VALUE;
+        int mid = low + (high - low)/2;
+        if(nums[mid] == target){
+            return Math.min(mid, binary(nums, low, mid-1, target));
+        }
+        if(nums[mid] > target){
+            return binary(nums, low, mid-1, target);
+        }
+        else if(nums[mid] < target){ 
+            return binary(nums, mid+1, high, target);
+        }
+        return Integer.MAX_VALUE;
+
+    }
+    public int binaryG(int[] nums,int low, int high, int target){
+        // int low = 0;
+        // int high = nums.length-1;
+        if(low > high) return -1;
+        int mid = low + (high - low)/2;
+        if(nums[mid] == target){
+            return Math.max(mid, binaryG(nums, mid+1, high, target));
+        }
+        if(nums[mid] > target){
+            return binaryG(nums, low, mid-1, target);
+        }
+        else if(nums[mid] < target){
+            return binaryG(nums, mid+1, high, target);
+        }
+        return -1;
+
     }
 }
