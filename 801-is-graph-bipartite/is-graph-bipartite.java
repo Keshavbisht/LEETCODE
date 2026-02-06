0c0d4469b1a0b5ccfdb4a8a9800bd9e6bca3033f
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-02-06 19:26:13 +0530

    Time: 11 ms (7.01%) | Memory: 47.7 MB (5.04%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,42 +1,51 @@
 class Solution {
-    int color[];
-    boolean vis[];
+    boolean[] vis;
+    int[] color;
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        color = new int[n];
         vis = new boolean[n];
+        color = new int[n];
         Arrays.fill(color, -1);
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0 ; i< n; i++){
-            adj.add(new ArrayList<>());
-        }
-
+        // Queue<int[]> q = new LinkedList<>();
         for(int i = 0; i< n; i++){
-            for(int j =0; j< graph[i].length; j++){
-                adj.get(i).add(graph[i][j]);
+            if(!vis[i]){
+                System.out.println(i);
+                if(!bfs(i, graph)) return false;
             }
-        }
-
-        for(int i = 0; i< n; i++){
-            if(!vis[i] ){
-                if(!dfs(i, 1, 0, adj)) return false;
-            }
+            
         }
         return true;
 
     }
-    public boolean dfs(int ind, int currColor, int colorT, ArrayList<ArrayList<Integer>> adj){
-        if(vis[ind] == true) return true;
-        color[ind] = currColor;
-        vis[ind] = true;
-        for(int i = 0; i< adj.get(ind).size(); i++){
-            int temp = adj.get(ind).get(i);
-            if(color[temp] == -1){
-                if(!dfs(temp, colorT, currColor, adj)) return false;
+    public boolean bfs(int ind, int[][] graph){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{ind, 0});
+
+        while(!q.isEmpty()){
+            int[] curr = q.remove();
+            int u = curr[0];
+            color[u] = curr[1];
+            vis[u] = true;
+
+            for(int i = 0; i< graph[u].length; i++){
+                if(color[u] == 0){
+                    if(color[u] == color[graph[u][i]]) return false;
+                    if(color[graph[u][i]] == -1 && !vis[graph[u][i]]){
+                        q.add(new int[]{graph[u][i], 1});
+                    }
+                    
+                }
+                else if(color[u] == 1){
+                    if(color[u] == color[graph[u][i]]) return false;
+                    // q.add(new int[]{graph[u][i], 0});
+                    if(color[graph[u][i]] == -1 && !vis[graph[u][i]]){
+                        q.add(new int[]{graph[u][i], 0});
+                    }
+                }
             }
-            else if(color[temp] == color[ind]) return false;
         }
         return true;
+
     }
 }
