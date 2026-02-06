f79c0333bf5e19eeb0c77f2a95ab38516e7b3643
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-02-06 12:16:26 +0530

    Time: 7 ms (9.84%) | Memory: 47.3 MB (53.64%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,36 +1,42 @@
 class Solution {
-    boolean[] vis;
-    int[] color;
+    int color[];
+    boolean vis[];
     public boolean isBipartite(int[][] graph) {
- 
         int n = graph.length;
-        // for(int i = 0; i< n; i++){
-        //     Arrays.sort(graph[i]);
-        // }
-        vis = new boolean[n];
         color = new int[n];
+        vis = new boolean[n];
         Arrays.fill(color, -1);
-        // boolean ans =  helper(0, 1,graph);
-        // if(n%2 != 0) return false;
-        for(int i = 0; i< n;i++){
-            if(color[i] == -1){
-                if(!helper(i, 1, graph)) return false;
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i< n; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i< n; i++){
+            for(int j =0; j< graph[i].length; j++){
+                adj.get(i).add(graph[i][j]);
             }
         }
 
+        for(int i = 0; i< n; i++){
+            if(!vis[i] ){
+                if(!dfs(i, 1, 0, adj)) return false;
+            }
+        }
         return true;
-        
+
     }
-    public boolean helper(int ind,int par, int[][] graph){
+    public boolean dfs(int ind, int currColor, int colorT, ArrayList<ArrayList<Integer>> adj){
+        if(vis[ind] == true) return true;
+        color[ind] = currColor;
         vis[ind] = true;
-        color[ind] = 1 - par;
-        for(int i = 0; i< graph[ind].length; i++){
-            if(color[ind] == color[graph[ind][i]]) return false;
-            if(vis[graph[ind][i]] == false){
-                if(!helper(graph[ind][i], color[ind], graph)) return false;
+        for(int i = 0; i< adj.get(ind).size(); i++){
+            int temp = adj.get(ind).get(i);
+            if(color[temp] == -1){
+                if(!dfs(temp, colorT, currColor, adj)) return false;
             }
+            else if(color[temp] == color[ind]) return false;
         }
         return true;
     }
-
 }
