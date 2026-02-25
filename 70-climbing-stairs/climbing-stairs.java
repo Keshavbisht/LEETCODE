b3a3e32d1fe98dad4e1d6b4506d9dd8ec36a08b0
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-02-25 11:15:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (54.83%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,14 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return fib(n, dp);
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for(int i = 0; i< n; i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;
     }
-    public int fib(int n ,int[] dp){
-        if(n<=1) return 1;
-        if(dp[n] != -1) return dp[n];
-        dp[n] = fib(n-1, dp) + fib(n-2, dp);
-        return dp[n];
-    }
-
+    
 }
