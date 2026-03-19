3cb8d1ed64a7a44d9a0957728b0c38c062dbfcb6
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-03-19 22:46:38 +0530

    Fix logic to handle n-repeated element detection

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -6,6 +6,7 @@ class Solution {
             if(!map.containsKey(nums[i])){
                 map.put(nums[i], 1);
             }
+            //else
             else{
                 map.put(nums[i], map.get(nums[i]) +1);
             }
