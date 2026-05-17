93f2ec14ea6b90faac10028afda7c0e5ad48ddf7
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-05-17 15:53:14 +0530

    Time: 5 ms (83.77%) | Memory: 48.3 MB (38.35%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -1,43 +1,60 @@
 class Solution {
     int ans = 0;
-    boolean[][] vis;
+    
     public int nearestExit(char[][] maze, int[] entrance) {
         int n = maze.length;
+        // System.out.println(n);
+        
         int m = maze[0].length;
+        // System.out.println(m);
+        int[][] vis = new int[n+1][m+1];
+        for(int[] itr : vis){
+            Arrays.fill(itr, -1);
+        }
+
+        // int[][] check = new int[][]
         int[][] check = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
-        vis = new boolean[n][m];
-        // if(!helper(entrance[0], entrance[1], maze)) return -1;
+    
+
         Queue<int[]> q = new LinkedList<>();
         q.add(entrance);
-        vis[entrance[0]][entrance[1]] = true;
-        q.add(new int[]{-1,-1});
+        q.add(new int[]{-1, -1});
+        vis[entrance[0]][entrance[1]] = 1;
+
+
         while(!q.isEmpty()){
             int[] curr = q.remove();
-            int i = curr[0];
-            int j = curr[1];
-            if(i == 0 || i == n-1 || j == 0|| j == m-1){
-                if(curr != entrance) return ans;
-            }
-            if(i == -1){
+            int u = curr[0];
+            int v = curr[1];
+
+            if(u == -1 && v == -1){
                 if(q.isEmpty()) return -1;
-                q.add(new int[]{-1, -1});
                 ans++;
+                q.add(new int[]{-1, -1});
             }
             else{
-                for(int a = 0; a<4; a++ ){
-                    if(i+check[a][0] < 0|| i+check[a][0] >= n || j +check[a][1] <0 || j+check[a][1] >=m) continue;
-                    if(maze[i+check[a][0]][j+check[a][1]]== '.' && !vis[i+check[a][0]][j+check[a][1]]){
-                        q.add(new int[]{i+check[a][0], j+check[a][1]});
-                        vis[i+check[a][0]][j+check[a][1]] = true;
+                
+                for(int[] itr: check){
+                    int temp1 = u + itr[0];
+                    int temp2 = v + itr[1];
+                    // System.out.println(temp1);
+                    // System.out.println(temp2);
+
+                    if( (temp1 > n-1 || temp1 < 0 || temp2 > m-1 || temp2 < 0)){
+                        if(ans ==0 ) continue;
+                        return ans;
                     }
+                    else{
+                        if(vis[temp1][temp2] == -1 && maze[temp1][temp2] != '+')
+                        {
+                            q.add(new int[]{temp1, temp2});
+                            vis[temp1][temp2] = 1;
+                        }
+                    }
+                    
                 }
             }
-
         }
-        return ans;
+        return -1;
     }
-    // public boolean helper(int i,int j, int ans, char[][] maze){
-    //     vis[i][j] = true;
-    //     if(i == 0 || i == n-1 || j == 0|| j == m-1) return true
-    // }
 }
