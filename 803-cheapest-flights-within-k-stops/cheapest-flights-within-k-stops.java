7518a2a84a3059ca7c1a0bcdc194792daa6d41af
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-05-18 08:13:25 +0530

    Time: 15 ms (8.75%) | Memory: 46.7 MB (24.56%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,41 +1,70 @@
 class Solution {
-    Integer[][] dp;
-
+    int ans = Integer.MAX_VALUE;
+    int level = 0;
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-
-        List<List<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for(int i = 0; i< n; i++){
             adj.add(new ArrayList<>());
         }
-        for (int[] f : flights) {
-            adj.get(f[0]).add(new int[]{f[1], f[2]});
+        int[] checkPoint = new int[n];
+        Arrays.fill(checkPoint, Integer.MAX_VALUE);
+        for(int i = 0 ; i< n ;i++){
+            System.out.println(checkPoint[i]+" ");
         }
-        dp = new Integer[n][k + 2];
-        int ans = dfs(src, dst, k + 1, adj);
-        return ans == Integer.MAX_VALUE ? -1 : ans;
-    }
+        
 
-    public int dfs(int node, int dst, int stops, List<List<int[]>> adj) {
+        for(int i = 0; i< flights.length; i++){
+            int u = flights[i][0];
+            int v = flights[i][1];
+            adj.get(u).add(new int[]{v, flights[i][2]});
+            // System.out.println(adj.get(u).get(0)[0] + " " + adj.get(u).get(0)[1]);
+        }
+        // for(int i = 0; i< n; i++){
+        //     for(int j = 0; j< adj.get(i).size(); j++){
+        //         System.out.println(adj.get(i).get(j)[0] + " " + adj.get(i).get(j)[1]);
+        //     }
+        // }
 
-        if (node == dst) return 0;
-        if (stops == 0) return Integer.MAX_VALUE;
 
-        if (dp[node][stops] != null)
-            return dp[node][stops];
 
-        int minCost = Integer.MAX_VALUE;
+        Queue<int[]> q = new LinkedList<>();
 
-        for (int[] nei : adj.get(node)) {
-            int next = nei[0];
-            int price = nei[1];
+        q.add(new int[]{src, 0});
+        q.add(new int[]{-1});
 
-            int cost = dfs(next, dst, stops - 1, adj);
+        int counter = 0;
+        while(!q.isEmpty()){
+            counter++;
+            // System.out.println(counter);
+            int[] curr = q.remove();
 
-            if (cost != Integer.MAX_VALUE) {
-                minCost = Math.min(minCost, price + cost);
+            if(curr[0] == -1){
+                q.add(new int[]{-1});
+                if(level > k || q.isEmpty())  return (ans == Integer.MAX_VALUE) ? -1: ans;
+                level++;
             }
+
+            else{
+                if(curr[0] == dst){
+                    ans = Math.min(ans, curr[1]);
+                }
+                for(int i = 0; i< adj.get(curr[0]).size(); i++){
+                    int[] temp = adj.get(curr[0]).get(i);
+                    // System.out.println(temp[1]);
+
+                    if(curr[1] + temp[1] <= checkPoint[temp[0]]){
+                        // System.out.println("PLEASE");
+                        q.add(new int[]{temp[0], curr[1] + temp[1]});
+                        checkPoint[temp[0]] = curr[1] + temp[1];
+                    }
+                }
+            }
+            
         }
 
-        return dp[node][stops] = minCost;
+        
+            
+        
+        return (ans == Integer.MAX_VALUE) ? -1: ans;
     }
 }
