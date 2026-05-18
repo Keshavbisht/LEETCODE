2db7fef985c3ab0b38ddbc571b38bc7ae33fa21d
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-05-18 10:41:10 +0530

    Time: 17 ms (40.76%) | Memory: 47.4 MB (86.73%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,39 +1,52 @@
 class Solution {
-    boolean[][] vis;
+    int level = 1;
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if(grid[0][0] == 1) return -1;
-        vis = new boolean[grid.length][grid[0].length];
+        
+        int[][] check = new int[][]{{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1}};
+        
+
+        
+        int n = grid.length;
+        
+        if(grid[n-1][n-1] == 1 || grid[0][0] == 1) return -1;
+        if(n == 1) return 1;
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{0,0});
-        q.add(new int[]{-1,-1});
-
-        int ans = 1;
-        int[][] direction = new int[][]{{1, 0},{0, 1},{-1, 0},{0, -1},{1, 1},{-1, -1},{1, -1},{-1, 1}};
+        q.add(new int[]{-1, -1});
+        
+        boolean[][] vis = new boolean[n+1][n+1];
+        vis[0][0] = true;
 
         while(!q.isEmpty()){
             int[] curr = q.remove();
             int i = curr[0];
             int j = curr[1];
-            if(i== grid.length -1 && j == grid.length-1) return ans;
-            if(i == -1){
+            if(i ==n-1 && j == n-1) return level;
+            if(curr[0] == -1){
                 if(q.isEmpty() && !vis[grid.length-1][grid.length-1]) return -1;
-                
-                ans++;
-                q.add(new int[]{-1,-1});
-                
+                q.add(new int[]{-1, -1});
+                level++;
             }
             else{
-                for(int a = 0; a< direction.length; a++){
-                    int u = direction[a][0];
-                    int v = direction[a][1];
-                    if(i+u <0 || i+u>=grid.length || j+ v <0 || j+v>=grid[0].length) continue;
-                    if(grid[i+u][j+v] == 0 && !vis[i+u][j+v]){
-                        q.add(new int[]{i+u, j+v});
-                        vis[i+u][j+v] = true;
+                for(int[] itr: check){
+                    int u = itr[0];
+                    int v = itr[1];
+                    
+                    if(i+u == n-1 && j+v == n-1) return level+1;
+                    // System.out.println((i+u)+ " "+ (j+v));
+                    if(i+u < n && i+u >=0 && j+v < n && j+v >= 0){
+                        // System.out.println("YAA2");
+                        if(!vis[i+u][j+v] && grid[i+u][j+v] == 0){
+                            
+                            q.add(new int[]{i+u, j+v});
+                            // System.out.println("YAA");
+                            vis[i+u][j+v] = true;
+                        }
                     }
                 }
             }
         }
         return -1;
+
     }
 }
