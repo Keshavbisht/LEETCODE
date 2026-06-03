9f1060c83661589f992cef7338117cc172a49000
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-06-03 17:35:40 +0530

    Time: 10 ms (9.32%) | Memory: 67.3 MB (15.11%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
@@ -14,51 +14,47 @@
  * }
  */
 class Tuple{
-    int data;
-    int currSum;
-    int LMax;
-    int RMin;
-    boolean flag;
+    int val;
+    int maxi = 0;
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+    int currSum = 0;
+    boolean flag = true;
     Tuple(){
-        data = 0;
-        currSum = 0;
-        LMax = Integer.MIN_VALUE;
-        RMin = Integer.MAX_VALUE;
-        flag = true;
+        this.val = 0;
+        // int lMax = Integer.MIN_VALUE;;
+        // int rMin = Integer.MAX_VALUE;
+        // int currSum = 0;
+        // boolean flag = true;
     }
-    Tuple(int x){
-        this.data = x;
-        currSum = 0;
-        LMax = Integer.MIN_VALUE;
-        RMin = Integer.MAX_VALUE;
-        flag = true;
+    Tuple(int val){
+        this.val = val;
     }
-
 }
 class Solution {
-    int maxi = 0;
     public int maxSumBST(TreeNode root) {
-        helper(root);
-        return maxi;
-        
+        Tuple temp = helper(root);
+        return temp.maxi;
     }
     public Tuple helper(TreeNode root){
         if(root == null) return new Tuple();
-        
-        Tuple curr = new Tuple(root.val);
 
+        Tuple curr = new Tuple(root.val);
 
         Tuple left = helper(root.left);
         Tuple right = helper(root.right);
-        if(!left.flag || !right.flag ||left.LMax >= root.val || right.RMin <= root.val){
+
+        if(left.flag == false || right.flag == false || left.max >= curr.val || right.min <= curr.val){
             curr.flag = false;
+            curr.maxi = Math.max(right.maxi, left.maxi);
             return curr;
         }
-        curr.currSum = left.currSum + right.currSum + root.val;
-        maxi = Math.max(maxi, curr.currSum);
-        curr.LMax = Math.max(right.LMax, root.val);
-        curr.RMin = Math.min(left.RMin, root.val);
 
+        curr.currSum = curr.val + left.currSum + right.currSum;
+        curr.maxi = Math.max(curr.currSum, Math.max(left.maxi, right.maxi));
+
+        curr.min = Math.min(curr.val, left.min);
+        curr.max = Math.max(curr.val, right.max);
         return curr;
 
     }
