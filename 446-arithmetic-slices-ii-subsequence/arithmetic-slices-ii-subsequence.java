220231213ce7ad53acc49df97cad17e4b3a60815
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-06-23 17:28:05 +0530

    Time: 205 ms (15.86%) | Memory: 110.3 MB (10.76%) - LeetSync

--- 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.java
+++ 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.java
@@ -1,34 +1,32 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         int n = nums.length;
-
         HashMap<Long, Integer>[] dp = new HashMap[n];
-
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i<n; i++){
             dp[i] = new HashMap<>();
         }
 
         int ans = 0;
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i< n; i++){
 
-            for (int j = 0; j < i; j++) {
+            for(int j = 0; j< i; j++){
 
-                long diff = (long) nums[j] - nums[i];
+                long temp = (long) nums[i] - nums[j];
 
- 
-                int old = dp[j].getOrDefault(diff, 0);
-
-
-                ans += old;
-                dp[i].put(
-                    diff,
-                    dp[i].getOrDefault(diff, 0)
-                    + old + 1
-                );
+                if(dp[j].containsKey(temp)){
+                    ans = ans + dp[j].get(temp);
+                }
+                dp[i].put(temp, dp[i].getOrDefault(temp, 0)+dp[j].getOrDefault(temp, 0)+1);
             }
         }
 
+        // for(int i = 0; i< n; i++){
+        //     System.out.println(i);
+        //     for(Map.Entry<Long, Integer> entry: dp[i].entrySet()){
+        //         System.out.println(entry.getKey()+" "+entry.getValue());
+        //     }
+        // }
         return ans;
     }
 }
