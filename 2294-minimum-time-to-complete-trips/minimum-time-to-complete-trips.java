5da91ba36df4d6854cb25ce30ab6853696c5276e
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-06-28 17:48:53 +0530

    Time: 82 ms (14.52%) | Memory: 91 MB (75.84%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
@@ -1,31 +1,36 @@
 class Solution {
-    public long minimumTime(int[] time, int totalTrips) {
-        long low = Long.MAX_VALUE;
+    public long minimumTime(int[] time, int h) {
+        long low = 1;
         long high = -1;
-        for(int itr: time){
-            low = Math.min(low, itr);
-            high = Math.max(high, itr);
+        for(int i = 0; i< time.length; i++){
+            high = Math.max(high, time[i]);
         }
-        high = high * totalTrips;
-        long ans = high;
+        high = high * h;
+        long ans = 0;
 
-        while(low <= high){
-            long mid = low + (high -low)/2;
+        while(low<= high){
+            long mid = low + (high - low)/2;
 
-            long temp = 0;
-            for(int i = 0; i< time.length; i++){
-                temp = temp + mid/time[i];
-            }
-            if(temp >=totalTrips ){
+            boolean flag = helper(mid, h, time);
+            if(flag){
                 ans = mid;
-                high = mid -1;
-
+                high = mid-1;
             }
             else{
                 low = mid+1;
             }
         }
         return ans;
-        
+    }
+
+    public boolean helper(long a, long h, int[] time){
+        long ans = 0;
+        for(int i = 0; i< time.length; i++){
+            
+            ans = ans + (a/time[i]);
+        }
+        if(ans >= h) return true;
+        return false;
+
     }
 }
