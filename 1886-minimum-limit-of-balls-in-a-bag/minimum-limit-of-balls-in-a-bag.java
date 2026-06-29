60f64cde357ebe24f9222642d1225b092c0d619a
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-06-29 16:47:11 +0530

    Time: 90 ms (6.76%) | Memory: 69.8 MB (59.59%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
@@ -1,34 +1,40 @@
 class Solution {
-    public int minimumSize(int[] nums, int maxOperations) {
+    int maxi = 0;
+    public int minimumSize(int[] nums, int m) {
         int low = 1;
-        int high = Integer.MIN_VALUE;
-        for(int i = 0; i< nums.length; i++ ){
+        int high = -1;
+
+        for(int i = 0; i< nums.length; i++){
             high = Math.max(high, nums[i]);
         }
-        int ans = high;
-        // int maxi = high;
+        int ans = 0;
+        maxi =high;
 
-        while(low<= high){
-            int mid = low + (high-low)/2;
-            boolean flag = helper(nums, maxOperations, mid);
-            if(flag == true){
-                high = mid-1;
+        while(low <= high){
+            int mid = low + (high -low)/2;
+
+            boolean flag = helper(mid, nums, m);
+            if(flag){
                 ans = mid;
+                high = mid-1;
             }
             else{
                 low = mid+1;
             }
         }
         return ans;
-    }
-    public boolean helper(int[] nums, int n, int mid){
-        // if(maxi % mid != 0) return false;
-        // if((maxi/mid)-1 > n) return false;
-        for(int i = 0; i< nums.length; i++){
-            n = n - (int)(Math.ceil((double)nums[i]/ mid)-1);
-            if(n <0) return false;
-        }
-        return true;
 
+    }
+    public boolean helper(int a, int[] nums, int m){
+        System.out.println(a);
+        for(int i = 0; i< nums.length; i++){
+             m -= (int) Math.ceil((double)nums[i]/ a) -1;
+             if(m < 0) return false;
+        }
+        // System.out.println(temp);
+        // if(temp <= m) return true;
+        if(m < 0) return false;
+
+        return true;
     }
 }
