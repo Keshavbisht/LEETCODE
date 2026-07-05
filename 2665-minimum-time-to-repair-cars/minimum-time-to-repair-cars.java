1695fa5790ddc66a86cd1b828a1d7f9592c60cb9
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-07-05 11:53:10 +0530

    Time: 35 ms (55.19%) | Memory: 58.5 MB (30.55%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
@@ -1,42 +1,37 @@
 class Solution {
     public long repairCars(int[] ranks, int cars) {
         long low = 1;
-        
-        long maxRank = 0;
-        for (int rank : ranks) {
-            maxRank = Math.max(maxRank, rank);
+        long high = -1;
+        for(int i = 0; i< ranks.length; i++){
+            high = Math.max(high, ranks[i]);
         }
-        long high = maxRank * (long)cars * cars;
-        
-        long ans = high;
-        
-        while (low <= high) {
-            long mid = low + (high - low) / 2;
-            
-            if (canRepairInTime(ranks, cars, mid)) {
+        high = high * cars * cars;
+        System.out.println(high);
+        long ans = 0;
+
+        while(low <= high){
+            long mid = low + (high-low)/2;
+
+            boolean flag = helper(mid, ranks, cars);
+            if(flag){
                 ans = mid;
-                high = mid - 1;  
-            } else {
-                low = mid + 1;   
+                high = mid-1;
+
+            }
+            else{
+                low = mid+1;
             }
         }
-        
         return ans;
     }
-    
-    private boolean canRepairInTime(int[] ranks, int cars, long time) {
-        long totalCars = 0;
-        
-        for (int rank : ranks) {
-            
-            long maxCars = (long)Math.sqrt(time / rank);
-            totalCars += maxCars;
-            
-            if (totalCars >= cars) {
-                return true;  
-            }
+    public boolean helper(long a, int[] nums, int cars){
+        long ans = 0;
+
+        for(int i = 0; i< nums.length; i++){
+            double temp = Math.sqrt(a/nums[i]);
+            ans += (int) temp;
+            if(ans >= cars) return true;
         }
-        
-        return totalCars >= cars;
+        return false;
     }
 }
