19a0bdb15d4b17f119ee2066e9c3e2a2cdd4a42e
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-07-06 18:12:10 +0530

    Time: 9 ms (82.26%) | Memory: 47.7 MB (29.57%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.java
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -1,19 +1,32 @@
 class Solution {
-    public long minNumberOfSeconds(int height, int[] times) {
-        long lo = 1;
-        long hi = 10000000000000000L;
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+        // n * (k(k+1)/2) = ans;
+        long low = 1;
+        long high = 10000000000000000L;
+        long ans = 0;
 
-        while (lo < hi) {
-            long mid = (lo + hi) >> 1;
-            long tot = 0;
-            for (int i = 0; i < times.length && tot < height; i++)
-                tot += (long) (Math.sqrt((double) mid / times[i] * 2 + 0.25) - 0.5);
-            if (tot >= height)
-                hi = mid;
-            else
-                lo = mid + 1;
+        while(low <= high){
+            long mid = low + (high-low)/2;
+
+            boolean flag = helper(mid, mountainHeight, workerTimes);
+            if(flag){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low= mid+1;
+            }
         }
+        return ans;
+    }
+    public boolean helper(long a, int n, int[] nums){
+        long count = 0;
 
-        return lo;
+        for(int i = 0; i< nums.length; i++){
+            double temp = (-1 + Math.sqrt(1 + (8.0 * a) / nums[i])) / 2.0;
+            count += (long) temp;
+            if(count >= n) return true;
+        }
+        return false;
     }
 }
