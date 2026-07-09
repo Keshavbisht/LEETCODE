be0dd2e0acb8a1b6b526709620cb8d881662c273
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-07-09 19:05:41 +0530

    Time: 447 ms (13.01%) | Memory: 288.6 MB (19.13%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.java
+++ 552-student-attendance-record-ii/student-attendance-record-ii.java
@@ -1,30 +1,26 @@
 class Solution {
     int MOD = 1000000007;
+    long dp[][][];
     public int checkRecord(int n) {
-        long[][][] dp = new long[n+1][2+1][3+1];
-        for(long[][] itr1 : dp){
-            for(long[] itr2: itr1){
-                Arrays.fill(itr2, -1);
+        dp = new long[n+1][2+1][3+1];
+        for(long[][] i: dp){
+            for(long[] itr: i){
+                Arrays.fill(itr, -1);
             }
         }
-
-        return (int) helper(0, n, 0, 0, dp) % MOD;
-
+        return (int) helper(0, n, 0, 0) % MOD;
     }
-    public long helper(int ind, int n, int countAbs, int contLate , long[][][] dp){
-        if(countAbs >= 2 || contLate >=3) return 0;
+    public long helper(int ind, int n, int countAbs, int late){
+        if(countAbs >= 2 || late >= 3) return 0;
         if(ind == n) return 1;
-        if(dp[ind][countAbs][contLate] != -1) return dp[ind][countAbs][contLate];
+        if(dp[ind][countAbs][late] != -1) return dp[ind][countAbs][late];
+        if(ind > n) return 0;
 
-        long a = 0;
-        long b= 0;
-        long c = 0;
 
-        
-        a = helper(ind+1, n, countAbs, 0, dp) %MOD;
-        b = helper(ind+1, n, countAbs +1, 0, dp) %MOD;
-        c = helper(ind+1, n, countAbs, contLate+1, dp) %MOD;
-        
-        return dp[ind][countAbs][contLate] = (a+b+c) %MOD;
+        long a = helper(ind+1, n, countAbs, 0) % MOD;
+        long b = helper(ind+1, n, countAbs+1, 0) % MOD;
+        long c = helper(ind+1, n, countAbs, late+1) % MOD;
+
+        return dp[ind][countAbs][late] = (a+b+c) % MOD;
     }
 }
