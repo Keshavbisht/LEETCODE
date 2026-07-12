57cc9fa3a7d8a87ddf8fb10a6ad3d04fa05ca51b
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-07-12 10:03:50 +0530

    Time: 1 ms (99.66%) | Memory: 47.3 MB (46.12%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,24 +1,27 @@
 class Solution {
+    int dp[][];
     public int minFallingPathSum(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int ans = Integer.MAX_VALUE;
-        int[][] dp = new int[m][n];
-        for(int[] itr: dp){
-            Arrays.fill(itr, -2);
+        dp = new int[matrix.length+1][matrix[0].length+1];
+        for(int[] itr : dp){
+            Arrays.fill(itr, -10000);
         }
-
-        for(int i = 0; i< n; i++){
-            ans = Math.min(ans, helper(0, i, matrix, dp));
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i< matrix.length; i++){
+            ans = Math.min(ans, helper(0, i, matrix));
         }
         return ans;
     }
-    public int helper(int i, int j, int[][] matrix, int[][] dp){
-        if(i>=matrix.length || j<0 || j>=matrix[0].length) return Integer.MAX_VALUE;
-        if(dp[i][j] != -2) return dp[i][j];
-        if(i == matrix.length-1) return matrix[i][j];
-        
-        
-        return dp[i][j] = matrix[i][j] + Math.min(helper(i+1, j, matrix, dp), Math.min(helper(i+1, j+1, matrix, dp), helper(i+1,j-1, matrix, dp)));
+    public int helper(int i,int j,  int[][] matrix){
+        if(i == matrix.length-1 && (j>=0) && j<matrix[0].length) return matrix[i][j];
+        if(i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length) return Integer.MAX_VALUE;
+        if(dp[i][j] != -10000) return dp[i][j];
+
+        int a = helper(i+1, j+1, matrix);
+        int b = helper(i+1, j, matrix);
+        int c = helper(i+1, j-1, matrix);
+
+        return dp[i][j] = Math.min(a, Math.min(b,c)) + matrix[i][j];
+
+
     }
 }
