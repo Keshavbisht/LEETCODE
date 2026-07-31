053857bbbf0d7a5cc0fa537cdf05818d8d3d96f7
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-07-31 21:40:15 +0530

    Time: 1 ms (100.00%) | Memory: 47.7 MB (48.60%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
@@ -14,27 +14,18 @@
  * }
  */
 class Solution {
-    int ans = 0;
     public int sumEvenGrandparent(TreeNode root) {
-        helper(root, 0, 0);
-        return ans;
+        return helper(root, -1, -1);
     }
+    public int helper(TreeNode root, int parent, int grandParent){
+        if(root == null) return 0;
 
-    public void helper(TreeNode root, int gParent, int parent){
-        if(root == null) return;
+        int left = helper(root.left, root.val, parent);
+        int right = helper(root.right, root.val, parent);
 
-        if(gParent == 1){
-            ans += root.val;
+        if(grandParent % 2 == 0){
+            return root.val + left+right;
         }
-
-        if(root.val %2 == 0){
-            helper(root.left, parent, 1);
-            helper(root.right, parent, 1);
-        }
-        else{
-            helper(root.left, parent, 0);
-            helper(root.right, parent, 0);
-        }
-
+        return left+right;
     }
 }
