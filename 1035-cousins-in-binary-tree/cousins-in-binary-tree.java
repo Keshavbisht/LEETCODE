4f058446f7cfc125d68c0c4e6da946a9f487a926
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-08-15 23:48:15 +0530

    Time: 1 ms (27.86%) | Memory: 43.2 MB (42.23%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -13,58 +13,56 @@
  *     }
  * }
  */
-class tuple {
-    TreeNode parent;
-    int level;
-    TreeNode current;
-    tuple(TreeNode current, TreeNode parent, int level){
-        this.current = current;
-        this.parent = parent;
-        this.level = level;
-    }
-}
-
 class Solution {
-    int level = 0;
-
+    class Tuple{
+        TreeNode root;
+        TreeNode par;
+        int level;
+        Tuple(TreeNode root, TreeNode par, int level){
+            this.root = root;
+            this.par = par;
+            this.level = level;
+        }
+    }
     public boolean isCousins(TreeNode root, int x, int y) {
-        Queue<tuple> q = new LinkedList<>();
-        tuple foundX = null;
-        tuple foundY = null;
         
-        q.add(new tuple(root, null, 0));
+        Queue<Tuple> q = new LinkedList<>();
+        int level = 0;
+        q.add(new Tuple(root, null, level));
         q.add(null);
+        Tuple t1 = null;
+        Tuple t2 = null;
 
-        while (!q.isEmpty()) {
-            if (foundX != null && foundY != null) {
-                if (foundX.level != foundY.level) return false;
-                if (foundX.parent == foundY.parent) return false;
-                return true;
+        while(!q.isEmpty()){
+            Tuple curr = q.remove();
+            if(curr == null){
+                if(t1 != null && t2 != null){
+                    if((t1.level == t2.level) && (t1.par.val != t2.par.val)) return true;
+                   
+                }
+                if(t1 != null || t2 != null) return false;
+                if(q.isEmpty()) return false;
+                q.add(null);
+                level++;
+                continue;
             }
-
-            tuple curr = q.remove();
-
-            if (curr == null) {
-                if (q.isEmpty()) return false;
-                else {
-                    level++;
-                    q.add(null);
+            else{
+                if(curr.root.val == x){
+                    t1 = curr;
                 }
-            } else {
-                if (curr.current.val == x) {
-                    foundX = curr;
-                } else if (curr.current.val == y) {
-                    foundY = curr;
+                if(curr.root.val == y){
+                    t2 = curr;
                 }
-
-                if (curr.current.left != null) {
-                    q.add(new tuple(curr.current.left, curr.current, curr.level + 1));
+                if(curr.root.left != null){
+                    q.add(new Tuple(curr.root.left, curr.root, level));
                 }
-                if (curr.current.right != null) {
-                    q.add(new tuple(curr.current.right, curr.current, curr.level + 1));
+                if(curr.root.right != null){
+                    q.add(new Tuple(curr.root.right, curr.root, level));
                 }
             }
+            
         }
         return false;
     }
+    
 }
