169432f3f7dd9eede3546d9102174dccc3bd9798
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-08-16 00:17:58 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (36.75%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.java
+++ 617-merge-two-binary-trees/merge-two-binary-trees.java
@@ -18,16 +18,15 @@ class Solution {
         return helper(root1, root2);
     }
     public TreeNode helper(TreeNode root1, TreeNode root2){
+        
         if(root1 == null) return root2;
-        if(root2 == null) return root1;
-
-        root1.val = root1.val + root2.val;
+        if(root2 == null) return  root1;
+        root1.val = root1.val +root2.val;
 
         root1.left = helper(root1.left, root2.left);
         root1.right = helper(root1.right, root2.right);
 
         return root1;
-
-        // return root1;
     }
+
 }
