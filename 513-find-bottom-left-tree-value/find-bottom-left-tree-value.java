57adc04ea9c16ed83c7ac71db0aa36af328ede4d
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2026-08-15 23:52:32 +0530

    Time: 4 ms (16.89%) | Memory: 46.6 MB (29.41%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -14,22 +14,31 @@
  * }
  */
 class Solution {
-    int[] ans = new int[]{-1,-1};
     public int findBottomLeftValue(TreeNode root) {
-        helper(root, 1,0 );
-        return ans[0];
-    }
-    public void helper(TreeNode root, int side, int level){
-        if(root == null) return;
-
-        
-        if(level > ans[1]){
-            ans[0] = root.val;
-            ans[1] = level;
+        Queue<TreeNode> q = new LinkedList<>();
+        int ans = -1;
+        q.add(root);
+        q.add(null);
+        boolean reset =false;
+        while(!q.isEmpty()){
+            TreeNode curr = q.remove();
+            if(curr == null){
+                if(q.isEmpty()) return ans;
+                q.add(null);
+                reset = false;
+                continue;
+            }
+            if(!reset){
+                ans = curr.val;
+                reset = true;
+            }
+            if(curr.left != null){
+                q.add(curr.left);
+            }
+            if(curr.right != null){
+                q.add(curr.right);
+            }
         }
-        
-
-        helper(root.left, 1, level+1);
-        helper(root.right, 0, level+1);
+        return ans;
     }
 }
